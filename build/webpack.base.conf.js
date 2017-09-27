4d676d393d55f6dc3b77a4861d63e22dfe65a2f5
wyg <290483181@qq.com>
2017-09-27 09:25:23 +0800

    利用缓存 解决首页返回 显示不正确问题

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -28,6 +28,7 @@ module.exports = {
       'components':resolve('src/components'),
       'api':resolve('src/api'),
       'router':resolve('src/router'),
+      'util':resolve('src/util')
     }
   },
   module: {
