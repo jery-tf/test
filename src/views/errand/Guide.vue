6a7383f4364aae0b226f257746a6ab252bacdd3c
xiaobai <695059346@qq.com>
2017-09-28 20:20:40 +0800

    修改便民页面组件，调整样式

--- src/views/errand/Guide.vue
+++ src/views/errand/Guide.vue
@@ -81,7 +81,6 @@
             }
         },
         created(){
-
         },
         methods: {
           testBtn(){
--- src/views/home/forpeople.vue
+++ src/views/home/forpeople.vue
@@ -6,104 +6,119 @@
   <div class="h100 doubleListView">
     <mt-tab-container v-model="selected">
       <mt-tab-container-item id="1">
-        <DoubleListView :dataList="leftDataList" :liClickFun="selecedLeftFun"
-                        :selectedId="selectedId">
+        <DoubleListView :dataList="leftDataList" :liClickFun="selecedLeftFun" :selectedId="selectedId">
           <div class="flex1 right">
-            <Education></Education>
+            <component v-bind:is="currentView" keep-alive>
+              <!-- 组件在 vm.currentview 变化时改变！ -->
+            </component>
           </div>
         </DoubleListView>
       </mt-tab-container-item>
-      <mt-tab-container-item id="2">
-        222
-      </mt-tab-container-item>
+
     </mt-tab-container>
   </div>
 </template>
 
 <script>
-  import DoubleListView from 'components/public/DoubleListView.vue'
-   import Education from 'components/convenpeo/Education.vue'
-  export default {
-    name: 'Errand',
-    components: {
-      DoubleListView,Education
-    },
-    data () {
-      return {
-        selected: '1',
-        leftDataList: [
-          {
-            id: 1,
-            icon: 'icon-ertongshouyang',
-            name: '婚育收养'
-          },{
-            id: 2,
-            icon: 'icon-xuexiao',
-            name: '教育培训'
-          },{
-            id: 3,
-            icon: 'icon-weibiaoti9',
-            name: '求职执业'
-          },
-          {
-            id: 4,
-            icon: 'icon-shui',
-            name: '纳税缴费'
-          },
-           {
-            id: 5,
-            icon: 'icon-jiuyifuwu',
-            name: '就医保健'
-          },
-          {
-            id: 6,
-            icon: 'icon-baoxianweidianji',
-            name: '社会保险'
-          },
-           {
-            id: 7,
-            icon: 'icon-kunnanjiuzhu',
-            name: '福利救助'
-          },
-            {
-            id: 8,
-            icon: 'icon-zufang',
-            name: '房屋租售'
-          },
-           {
-            id: 9,
-            icon: 'icon-bus',
-            name: '交通旅游'
-          },
-        ],
-        selectedId:1
-      }
-    },
-    created(){
+import DoubleListView from 'components/public/DoubleListView.vue'
+import Education from 'components/convenpeo/Education.vue'
+import Adoption from 'components/convenpeo/Adoption.vue'
+import ApplyJob from 'components/convenpeo/ApplyJob.vue'
+import Ratepay from 'components/convenpeo/Ratepay.vue'
+export default {
+  name: 'Errand',
+  components: {
+    DoubleListView,
+     Education:Education, 
+    Adoption:Adoption
+  },
+  data() {
+    return {
+      selected: '1',
+      currentView: 'Education',
+      leftDataList: [
+        {
+          id: 1,
+          icon: 'icon-ertongshouyang',
+          name: '婚育收养'
+        }, {
+          id: 2,
+          icon: 'icon-xuexiao',
+          name: '教育培训'
+        }, {
+          id: 3,
+          icon: 'icon-weibiaoti9',
+          name: '求职执业'
+        },
+        {
+          id: 4,
+          icon: 'icon-shui',
+          name: '纳税缴费'
+        },
+        {
+          id: 5,
+          icon: 'icon-jiuyifuwu',
+          name: '就医保健'
+        },
+        {
+          id: 6,
+          icon: 'icon-baoxianweidianji',
+          name: '社会保险'
+        },
+        {
+          id: 7,
+          icon: 'icon-kunnanjiuzhu',
+          name: '福利救助'
+        },
+        {
+          id: 8,
+          icon: 'icon-zufang',
+          name: '房屋租售'
+        },
+        {
+          id: 9,
+          icon: 'icon-bus',
+          name: '交通旅游'
+        },
+      ],
+      selectedId: 1
+    }
+  },
+  created() {
 
-    },
-    methods: {
-      //点击左侧列表 单元格
-      selecedLeftFun(id){
-          //拿着ID 请求数据
-        console.log('selecedLeftFun-->',id);
-        this.selectedId = id;
+  },
+  methods: {
+    //点击左侧列表 单元格
+    selecedLeftFun(id) {
+      //左侧列表发生变化  右侧组件变化成不同组件
+      this.selectedId = id;
+      if (id == "1") {
+        this.currentView=Education
+      }
+      if (id == "2") {
+        this.currentView=Adoption
+      }
+      if (this.selectedId == "3") {
+        this.currentView=ApplyJob
+      }
+       if (this.selectedId == "4") {
+        this.currentView=Ratepay
       }
     }
-
   }
+
+}
 </script>
 
 <style lang="less" rel="stylesheet/less">
-  .doubleListView {
-    display: flex;
-    flex-direction: column;
-    .mint-tab-container {
-      flex: 1;
-      .mint-tab-container-wrap {
-        height: 100%;
-      }
+.doubleListView {
+  display: flex;
+  flex-direction: column;
+  .mint-tab-container {
+    flex: 1;
+    .mint-tab-container-wrap {
+      height: 100%;
     }
   }
-
+}
 </style>
