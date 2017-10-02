98043e217f3278d7549f1db751c80ab3de07f69a
wyg <290483181@qq.com>
2017-10-02 09:19:49 +0800

    获取用户详情

--- src/api/user.js
+++ src/api/user.js
@@ -3,18 +3,16 @@
  */
 import { axiosGet ,axiosGetToken} from './baseApi'
 import Config from '../config';
-/***
- * 用户相关接口
- * 公共参数
- * @param loading [加载时显示的文字]
- */
 
 /***
  * 获取用户信息
  * @param params  access_token
- * @param loading
+ * @param config
  */
 export const getUserInfo = (params,config)=>axiosGetToken(`${Config.loginUrl}/oauth2/user_info`,params,config);
 
-
-export const getUserDetails = id=>axiosGetToken(`/uop/v1/users/${id}`)
+/***
+ * 根据Id 获取用户详情
+ * @param userId
+ */
+export const getUserDetails = userId=>axiosGetToken(`/uop/v1/users/${userId}`)
