edbc1e7b5220044480d06dbb502c0ed545b69ddd
xiaobai <695059346@qq.com>
2017-11-21 16:02:57 +0800

    便民修改

--- src/config.js
+++ src/config.js
@@ -5,12 +5,12 @@
 import dictionaries from './dictionaries'
 
 export default {
-  environment:'production',
-  apiUrl:'/proxy',
+  environment:'dev',
+  apiUrl:'/proxy', //代理 http://172.17.89.21:30538
   cmsUrl:'http://222.240.80.57',
-  loginUrl:'http://aip.egp.c2cloud.cn',
+  loginUrl:'http://172.17.89.21:30069',
   clientId:'JQNb9l66Qk22ibryPBATnOg',
   AppSignUrl:'http://172.16.17.20:8080',
+  C2XApiKey:'sPvuCaUsT2aBuricqor963A',
   Dictionaries:dictionaries
 }
-
