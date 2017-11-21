d6c5e8057111003bdd7fa32eeae48abab228fc9d
xiaobai <695059346@qq.com>
2017-11-21 14:24:28 +0800

    便民修改

--- src/config.production.js
+++ src/config.production.js
@@ -7,7 +7,7 @@ import dictionaries from './dictionaries'
 export default {
   environment:'production',
   apiUrl:'/proxy',
-  cmsUrl:'http://hnzwfwcms.s1.natapp.cc',
+  cmsUrl:'http://222.240.80.57',
   loginUrl:'http://aip.egp.c2cloud.cn',
   clientId:'JQNb9l66Qk22ibryPBATnOg',
   AppSignUrl:'http://172.16.17.20:8080',
